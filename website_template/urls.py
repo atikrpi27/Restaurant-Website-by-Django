429a5e8d29cb7d9a6eb634000b60e815efe7ce1c
atikrpi27 <atik.rpi27@gmail.com>
2022-01-11 13:55:32 +0600

    Django Static File update

--- website_template/urls.py
+++ website_template/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+
 apps_name='website_template'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('menu/', views.menu, name='menu'),
+    path('reservation/', views.reservation, name='reservation'),
+    path('team/', views.team, name='team'),
+    path('special_dishes/', views.special_dishes, name='special_dishes'),
 ]
--- website_template/views.py
+++ website_template/views.py
@@ -4,4 +4,19 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hi i\'m Atikur Rahman</h1>')
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def menu(request):
+    return render(request,'menu.html')
+
+def reservation(request):
+    return render(request,'reservation.html')
+
+def team(request):
+    return render(request,'team.html')
+
+def special_dishes(request):
+    return render(request,'special-dishes.html')
